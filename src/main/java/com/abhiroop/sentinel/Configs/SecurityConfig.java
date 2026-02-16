b4ebfda2029f4e732e3c5f19c6147efc1c57a818
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-02-16 21:30:49 +0530

    Add: cors config bean

--- src/main/java/com/abhiroop/sentinel/Configs/SecurityConfig.java
+++ src/main/java/com/abhiroop/sentinel/Configs/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -25,5 +30,23 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final var corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://sentinel-25902.web.app/",
+                "https://sentinel-25902.firebaseapp.com/"
+        ));
+
+        corsConfig.setAllowedMethods(List.of("GET"));
+
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
 }
