24901c599a822042e1dccd80d9354692f75fa8a6
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-10 20:37:13 +0530

    FIX: remove collection name link from LoginHistory entity to stop mistake write into firestore db (the feature is ported over to sentinel-ingestor)

--- src/main/java/com/abhiroop/sentinel/entity/LoginHistory.java
+++ src/main/java/com/abhiroop/sentinel/entity/LoginHistory.java
@@ -1,11 +1,9 @@
 package com.abhiroop.sentinel.entity;
 
 import com.google.cloud.firestore.annotation.DocumentId;
-import com.google.cloud.spring.data.firestore.Document;
 import lombok.Builder;
 
 @Builder(toBuilder = true)
-@Document(collectionName = "login_history")
 public record LoginHistory(
         @DocumentId
         String id,
